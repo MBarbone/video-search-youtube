66e594ae9e6dcc3defec06c3b44e9bb1ba6d9738
Mike <michael.barbone@gmail.com>
2019-08-20 15:27:41 -0400

    Rendering selected video within video player

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../api/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
   state = {
-    videos: []
+    videos: [],
+    selectedVideo: null
   };
 
   onTermSubmit = async term => {
@@ -19,11 +21,20 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = video => {
+    console.log("From the App", video);
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={this.onVideoSelect}
+        />
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,10 @@ import React from "react";
 
 const VideoItem = props => {
   return (
-    <div className="video-item item">
+    <div
+      className="video-item item"
+      onClick={() => props.onVideoSelect(props.video)}
+    >
       <img
         className="ui image"
         src={props.video.snippet.thumbnails.medium.url}
